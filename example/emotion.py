d93800045770a13a5b083ae763b84e4f33ea8f74
Gugo-le <rngustmd0719@naver.com>
2023-08-11 00:47:15 +0900

    separate test data

--- example/emotion.py
+++ example/emotion.py
@@ -1,9 +1,9 @@
 import numpy as np
-from sklearn.model_selection import train_test_split
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, LSTM, Dense
+import pickle
 
 # 텍스트 데이터와 라벨 데이터 예시
 texts = ["This movie was amazing!", "I didn't like the film at all."]
@@ -17,9 +17,6 @@ sequences = tokenizer.texts_to_sequences(texts)
 # 패딩
 padded_sequences = pad_sequences(sequences, maxlen=100, padding="post", truncating="post")
 
-# 훈련 데이터 및 테스트 데이터 분할
-X_train, X_test, y_train, y_test = train_test_split(padded_sequences, labels, test_size=0.2, random_state=42)
-
 # 모델 생성
 model = Sequential([
     Embedding(10000, 32, input_length=100),
@@ -32,16 +29,27 @@ model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"]
 model.summary()
 
 # 모델 훈련
-history = model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test), batch_size=128)
+model.fit(padded_sequences, labels, epochs=10, batch_size=128)
 
-# 모델 성능 평가
-loss, accuracy = model.evaluate(X_test, y_test)
-print(f"Loss: {loss}\nAccuracy: {accuracy}")
+# 모델 저장
+with open("tokenizer.pickle", "wb") as f:
+    pickle.dump(tokenizer, f)
+model.save("sentiment_model.h5")
 
-# 새로운 텍스트로 감정 예측
-new_text = ["This movie was amazing!"]
-new_sequence = tokenizer.texts_to_sequences(new_text)
-padded_new_sequence = pad_sequences(new_sequence, maxlen=100, padding="post", truncating="post")
-prediction = model.predict(padded_new_sequence)
-sentiment = "Positive" if float(prediction) >= 0.5 else "Negative"
-print(f"Sentiment: {sentiment} ({float(prediction):.4f})")
+# 모델 불러오기
+with open("tokenizer.pickle", "rb") as f:
+    tokenizer = pickle.load(f)
+model = load_model("sentiment_model.h5")
+
+# 감정 예측 함수
+def predict_sentiment(text):
+    new_sequence = tokenizer.texts_to_sequences([text])
+    padded_new_sequence = pad_sequences(new_sequence, maxlen=100, padding="post", truncating="post")
+    prediction = model.predict(padded_new_sequence)
+    sentiment = "Positive" if float(prediction) >= 0.5 else "Negative"
+    return sentiment, float(prediction)
+
+# 예측 예시
+text_to_predict = "This was a great movie!"
+sentiment, prediction = predict_sentiment(text_to_predict)
+print(f"Text: {text_to_predict}\nSentiment: {sentiment} ({prediction:.4f})")
